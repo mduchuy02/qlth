47fd399dbb7525f39496ce114d794fb91041b94b
Loc <anhlocok123sa1@gmail.com>
2024-06-19 00:59:10 +0700

    Tai khoan sinh vien controller

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\LichDayController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LopController;
 use App\Http\Controllers\TaiKhoanGVController;
+use App\Http\Controllers\TaiKhoanSVController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,14 +26,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 Route::middleware('auth:sanctum')->group(function () {
+    //Tai khoan giao vien
     Route::get('/taikhoangv', [LoginController::class, 'getUserByToken']);
-    Route::post('/logout', [TaiKhoanGVController::class, 'logout']);
     Route::get('/taikhoangvs', [TaiKhoanGVController::class, 'index']);
     Route::get('/taikhoangv/{id}', [TaiKhoanGVController::class, 'show']);
     Route::get('/taikhoangv/{id}/edit', [TaiKhoanGVController::class, 'edit']);
     Route::put('/taikhoangv/{id}', [TaiKhoanGVController::class, 'update']);
     Route::delete('/taikhoangv/{id}', [TaiKhoanGVController::class, 'destroy']);
     Route::post('/taikhoangv', [TaiKhoanGVController::class, 'store']);
+    //Tai khoan sinh vien
+    Route::get('/taikhoansvs', [TaiKhoanSVController::class, 'index']);
+    Route::delete('/taikhoansv/{id}', [TaiKhoanSVController::class, 'destroy']);
+    Route::get('/taikhoansv/{id}/edit', [TaiKhoansVController::class, 'edit']);
+    Route::put('/taikhoansv/{id}', [TaiKhoanSVController::class, 'update']);
+    Route::post('/taikhoansv', [TaiKhoanSVController::class, 'store']);
+    //Lop
+    Route::get('/lop', [LopController::class, 'index']);
+    //else
+    Route::post('/logout', [TaiKhoanGVController::class, 'logout']);
     Route::get('/hocky/{id}', [LichDayController::class, 'getHocKy']);
     Route::get('/lichgd/{ma_gv}', [LichDayController::class, 'getLichGD']);
 });
