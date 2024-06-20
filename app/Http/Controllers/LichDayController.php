f703bc29e77d639b5ac48d889a2c953ed9e205ab
Loc <anhlocok123sa1@gmail.com>
2024-06-20 08:41:10 +0700

    DiemDanhController

--- app/Http/Controllers/LichDayController.php
+++ app/Http/Controllers/LichDayController.php
@@ -16,43 +16,12 @@ class LichDayController extends Controller
     }
     public function getHocKy($ma_gv)
     {
-        $lichDays = LichDay::where('ma_gv', $ma_gv)->get();
-        $thoiGianHoc = [];
-        foreach ($lichDays as $lichDay) {
-            $thoiGian = explode('-', $lichDay->thoi_gian);
-            $ngayBatDau = date_create_from_format('d/m/Y', trim($thoiGian[0]));
-            $month = $ngayBatDau->format('m');
-            switch ($month) {
-                case 9:
-                case 10:
-                case 11:
-                case 12:
-                case 1:
-                    $hoc_ky = 1;
-                    break;
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                case 6:
-                    $hoc_ky = 2;
-                    break;
-                case 7:
-                case 8:
-                    $hoc_ky = 3;
-                    break;
-            }
-            $year = $ngayBatDau->format('Y');
-            $lichDay->hoc_ky = "Học kỳ " . $hoc_ky . " năm học " . $year . "-" . $year + 1;
-            array_push($thoiGianHoc, $lichDay->hoc_ky);
-            $querys = LichDay::where('ma_gv', $ma_gv)->get('hoc_ky');
-            foreach ($querys as $query) {
-                $hocky = str_split($query);
-                $query->hoc_ky_text = "Học kỳ " . $hocky[10] . " năm học 20" . $hocky[11] . $hocky[12] . "-20" . $hocky[11] . $hocky[12] + 1;
-            }
-            return $thoiGianHoc;
-            return response()->json($querys);
+        $querys = LichDay::where('ma_gv', $ma_gv)->get('hoc_ky');
+        foreach ($querys as $query) {
+            $hocky = str_split($query);
+            $query->hoc_ky_text = "Học kỳ " . $hocky[10] . " năm học 20" . $hocky[11] . $hocky[12] . "-20" . $hocky[11] . $hocky[12] + 1;
         }
+        return response()->json($querys);
     }
     public function getLichGD(Request $request, $magv)
     {
